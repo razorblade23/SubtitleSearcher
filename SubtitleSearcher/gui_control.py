49dc787cbef8a279b826b7b1e9137a5dbf90d476
razorblade23 <josip.vivo.cubric@gmail.com>
2021-11-07 12:45:28 +0100

    Commented test

--- SubtitleSearcher/gui_control.py
+++ SubtitleSearcher/gui_control.py
@@ -83,7 +83,7 @@ def search_by_single_file(values, lang, window):
         window['STATUSBAR'].update(f'Movie name: {movie.title} - IMDB ID: {movie.imdb_id}')
         link = opensubs.create_link(imdb=movie.imdb_id, bytesize=fileSize, hash=hashed_file, language=lang)
         subtitles = opensubs.request_subtitles(link)
-    subtitles=[] # Comment / Uncomment this to simulate finding hash failed
+    #subtitles=[] # Comment / Uncomment this to simulate finding hash failed
     all_subs = []
     if len(subtitles) == 0: # If finding movie with hash failed and list "subtitles" is empty so it length is 0
         movie_name = movie.title
